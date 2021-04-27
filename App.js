9d69fe4bab0a23f10b5ea2deb615452afc0277ff
Cherag Verma <cherag.verma@walmart.com>
2021-04-27 21:03:45 +0530

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { WebView } from "react-native-webview";
+import { Dimensions } from "react-native";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.webViewContainer}>
+          <WebView
+            source={{ uri: "http://www.numerologybit.com/" }}
+            style={styles.webView}
+          />
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    height: windowHeight,
+    width: windowWidth,
+  },
+  webViewContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  webView: {
+    height: windowHeight,
+    width: windowWidth,
+  },
+  adBanner: {
+    height: 52,
+    width: "100%",
+    position: "absolute",
+    backgroundColor: "red",
+    bottom: 0,
   },
 });
